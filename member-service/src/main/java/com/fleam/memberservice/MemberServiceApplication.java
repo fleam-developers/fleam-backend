0c3f1f3a916992ced80484fe7dbdb0e488a10360
selimseker <selim.seker00@gmail.com>
2021-12-19 12:46:53 +0300

    add gateway, eureka and member service (partially)

---
+++ member-service/src/main/java/com/fleam/memberservice/MemberServiceApplication.java
@@ -0,0 +1,18 @@
+package com.fleam.memberservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MemberServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberServiceApplication.class, args);
+	}
+
+}
